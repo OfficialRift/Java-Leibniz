a93ea7c909f7295fd81500431d4eaf28e36b5022
Sebastian Stenbom <ryezze@gmail.com>
2022-01-05 12:26:15 +0100

    more loops

--- Leibniz.java
+++ Leibniz.java
@@ -4,7 +4,7 @@ class piformula
         double pi = 0;
         double den = 1;
 
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 1000000000; i++)
         {
             if(i % 2 == 0)
             {
