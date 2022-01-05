e51dbdb54883e5287a1d18b2fa80dd08478a6862
Sebastian Stenbom <ryezze@gmail.com>
2022-01-05 12:13:26 +0100

    java code added

---
+++ Leibniz.java
@@ -0,0 +1,22 @@
+class piformula
+{
+    public static void main(String[] args) {
+        double pi = 0;
+        double den = 1;
+
+        for(int i = 0; i < 100; i++)
+        {
+            if(i % 2 == 0)
+            {
+                pi = pi + (1/den);
+            }
+            else
+            {
+                pi = pi - (1/den);
+            }
+            den = den +2;
+        }
+        pi = pi * 4;
+        System.out.println(pi);
+    }
+}
